19077e84f305542b07f0676ba4574a5bcfe4741c
Prem Singh <sincost550@gmail.com>
2026-08-20 16:16:43 +0530

    Time: 9 ms (4.5%), Space: 71.8 MB (53.08%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        if(nums.size()==1) return nums[0];
+        //KADANE'S ALGO
+        int sum=nums[0];
+        int mxsum=nums[0];
+        for(int i=1;i<nums.size();i++){
+            sum=max(nums[i],sum+nums[i]);
+            mxsum=max(mxsum,sum);
+        }
+        return mxsum;
+    }
+};
