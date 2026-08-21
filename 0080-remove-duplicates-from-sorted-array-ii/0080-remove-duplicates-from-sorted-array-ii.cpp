e13a8175d24e10d4234ebcc7c955cca0533b74f5
Prem Singh <sincost550@gmail.com>
2026-08-21 22:27:07 +0530

    Time: 9 ms (48.96%), Space: 19.7 MB (24.04%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        map<int,int> freq;
-        for(int num:nums){
-            freq[num]++;
-        }
-        int i=0;
-        for(auto const&[num,count]:freq){
-            int finalCount=min(count,2);
-            for(int j=0;j<finalCount;j++){
-                nums[i++]=num;
+        // map<int,int> freq;
+        // for(int num:nums){
+        //     freq[num]++;
+        // }
+        // int i=0;
+        // for(auto const&[num,count]:freq){
+        //     int finalCount=min(count,2);
+        //     for(int j=0;j<finalCount;j++){
+        //         nums[i++]=num;
+        //     }
+        // }
+        // return i;
+        if (nums.size() <= 2) return nums.size();
+        
+        int i = 2; // Pointer to place the next valid element
+        
+        for (int j = 2; j < nums.size(); j++) {
+            // Compare current element with element 2 positions back
+            if (nums[j] != nums[i - 2]) {
+                nums[i] = nums[j];
+                i++;
             }
         }
-        return i;
+        
+        return i; // Length of updated array
     }
 };
