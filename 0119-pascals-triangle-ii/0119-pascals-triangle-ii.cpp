876c5192926371fa0b0a73bea41da4dc67880930
Prem Singh <sincost550@gmail.com>
2026-08-22 13:08:50 +0530

    Time: 2 ms (9.42%), Space: 9.4 MB (5.76%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<vector<int>> res;
+        for(int i=0;i<=rowIndex;i++){
+            vector<int> v;
+            for(int j=0;j<=i;j++){
+                if(j==0||j==i){
+                    v.push_back(1);
+                }
+                else{
+                    v.push_back(res[i-1][j-1]+res[i-1][j]);
+                }
+            }
+            res.push_back(v);
+        }
+        return res[res.size()-1];
+    }
+};
