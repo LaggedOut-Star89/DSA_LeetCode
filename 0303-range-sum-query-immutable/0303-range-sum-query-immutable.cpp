e27791b52b210c47ffedfd195d3093d9578a2ef9
Prem Singh <sincost550@gmail.com>
2026-09-02 13:02:35 +0530

    Time: 0 ms (100%), Space: 23.9 MB (61.48%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -1,34 +1,34 @@
-// class NumArray {
-// public:
-//     vector<int> v;
-//     NumArray(vector<int>& nums) {
-//         int n=nums.size();
-//         v.resize(n+1,0);
-//         for(int i=0;i<n;i++){
-//             v[i+1]=v[i]+nums[i];
-//         }
-//     }
-    
-//     int sumRange(int left, int right) {
-//         return v[right+1]-v[left];
-//     }
-// };
-
 class NumArray {
 public:
-    vector<int> nums;
+    vector<int> v;
     NumArray(vector<int>& nums) {
-        this->nums=nums;
+        int n=nums.size();
+        v.resize(n+1,0);
+        for(int i=0;i<n;i++){
+            v[i+1]=v[i]+nums[i];
+        }
     }
     
     int sumRange(int left, int right) {
-        int sum=0;
-        for(int i=left;i<right+1;i++){
-            sum+=nums[i];
-        }
-        return sum;
+        return v[right+1]-v[left];
     }
 };
+
+// class NumArray {
+// public:
+//     vector<int> nums;
+//     NumArray(vector<int>& nums) {
+//         this->nums=nums;
+//     }
+    
+//     int sumRange(int left, int right) {
+//         int sum=0;
+//         for(int i=left;i<right+1;i++){
+//             sum+=nums[i];
+//         }
+//         return sum;
+//     }
+// };
 
 
 /**
