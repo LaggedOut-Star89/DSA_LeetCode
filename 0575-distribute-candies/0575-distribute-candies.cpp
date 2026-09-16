ee965ac2c70b7c67c6b7bd4744945f63ed5d258b
Prem Singh <sincost550@gmail.com>
2026-09-16 14:24:29 +0530

    Time: 106 ms (56.84%), Space: 120.1 MB (41.8%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.cpp
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -1,18 +1,25 @@
+// class Solution {
+// public:
+//     int distributeCandies(vector<int>& candyType) {
+//         map<int,int> mp;
+//         for(int num:candyType){
+//             mp[num]++;
+//         }
+//         int freqsum=0;
+//         for(auto x:mp){
+//             freqsum+=x.second;
+//         }
+//         if(mp.size()<freqsum/2){
+//             return mp.size();
+//         }
+//         return freqsum/2;
+
+//     }
+// };
 class Solution {
 public:
     int distributeCandies(vector<int>& candyType) {
-        map<int,int> mp;
-        for(int num:candyType){
-            mp[num]++;
-        }
-        int freqsum=0;
-        for(auto x:mp){
-            freqsum+=x.second;
-        }
-        if(mp.size()<freqsum/2){
-            return mp.size();
-        }
-        return freqsum/2;
-
+        unordered_set<int> st(candyType.begin(), candyType.end());
+        return min((int)st.size(), (int)candyType.size() / 2);
     }
 };
